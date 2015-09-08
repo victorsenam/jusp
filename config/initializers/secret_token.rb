90c63dbf6e8f83378fb187141adb89a29d57fdd2
victormatta <victormatta14@gmail.com>
2015-09-08 18:20:57 -0300

    rails new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Jusp::Application.config.secret_key_base = 'f8847e3562b20d9456aaafa13116c41c59a5fb61ae851ae2953afac73e926ad95ceb9ef5c0f08aa15741f94b0d4a65de676c5cbac110a266952be8388683eef4'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Jusp::Application.config.session_store :cookie_store, key: '_jusp_session'
