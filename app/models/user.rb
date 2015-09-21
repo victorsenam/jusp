5a6f6bc0b84bb18bd4efd2a623065c9745c30c3f
victormatta <victormatta14@gmail.com>
2015-09-21 12:40:07 -0300

    user model and tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,9 @@
 class User < ActiveRecord::Base
-    has_secure_password
+  has_secure_password
+  validates :name, presence: true, length: { maximum: 30 },
+                   uniqueness: { case_sensitive: false }
+  EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 255 },
+                    format: { with: EMAIL_REGEX }
+  validates :password, presence: true, length: { minimum: 6 }
 end
